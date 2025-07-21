9861198bf271200c603e5c6568908105677ec896
saurabh <saurabh.solanki@kevit.io>
2025-07-21 18:52:18 +0530

    contact page js added along with contact adding and deleting functionality

--- contact-page/contact.js
+++ contact-page/contact.js
@@ -1,6 +1,93 @@
 const addBtn = document.querySelector('.add-contact-btn');
 const formBox = document.querySelector('.contact-form-box')
+const body = document.querySelector('body');
+const cancelBtn = document.querySelector('.cancel');
+const addNewContBtn = document.querySelector('.add-new-cont');
+let data = [];
+let table = document.querySelector('table');
 
 addBtn.addEventListener('click', () => {
     formBox.classList.add('contact-form-visible');
+    body.classList.add('body-visible');
+    formBox.style.visibility = 'visible';
+    formBox.style.display = 'block';
 })
+
+cancelBtn.addEventListener('click', () => {
+    body.classList.remove('body-visible');
+    formBox.style.display = 'none';
+})
+
+addNewContBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    //hide contact form box
+    body.classList.remove('body-visible');
+    formBox.style.display = 'none';
+
+    //push in array
+    // let compName = document.querySelector('#company-name').value;
+    // let emailId = document.querySelector('#email').value;
+    // let phoneNum = document.querySelector('#phone').value;
+    // let tag = document.querySelector('#tags').value;
+
+    // let data_obj = {
+    //     company_name: compName,
+    //     email_id: emailId,
+    //     contact_number: phoneNum,
+    //     tag_name: tag
+    // }
+
+    // data.push(data_obj);
+    // console.log(data);
+
+    //table display
+    let tr = document.createElement('tr');
+    let nameTd = document.createElement('td');
+    let emTd = document.createElement('td');
+    let phnTd = document.createElement('td');
+    let tagTd = document.createElement('td');
+    let tagSpan = document.createElement('span');
+
+    tagSpan.innerText = tag;
+    tagSpan.classList.add('tag');
+    nameTd.innerText = compName;
+    emTd.innerText = emailId;
+    phnTd.innerText = phoneNum;
+
+    tagTd.appendChild(tagSpan);
+
+    let btnTd = document.createElement('td');
+    let edtBtn = document.createElement('button');
+    edtBtn.innerText = 'Edit';
+    edtBtn.classList.add('edit-btn');
+    let dltBtn = document.createElement('button');
+    dltBtn.innerText = 'Delete';
+    dltBtn.classList.add('delete-btn');
+
+    btnTd.appendChild(edtBtn);
+    btnTd.appendChild(dltBtn);
+
+    tr.appendChild(nameTd);
+    tr.appendChild(phnTd);
+    tr.appendChild(emTd);
+    tr.appendChild(tagTd);
+    tr.appendChild(btnTd);
+    table.appendChild(tr);
+
+    //delete row
+    let dltBtns = document.querySelectorAll('.delete-btn');
+    dltBtns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            e.target.parentElement.parentElement.remove();
+        })
+    })
+
+
+
+})
+
+
+
+
+
